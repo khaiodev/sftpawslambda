3bf5068620d072989f3849a84d57ec76523721ad
khaiolopes <93495165+khaiodev@users.noreply.github.com>
2022-09-20 15:11:57 -0300

    Update functionsftplambda.py

--- functionsftplambda.py
+++ functionsftplambda.py
@@ -1,17 +1,47 @@
+import json
+import os
 import pysftp as sftp
+import urllib.parse
+import boto3
 
 FTP_HOST = "valueFTP_HOST"
 FTP_USER = "valueFTP_USER"
-FTP_PASS = "valueFTP_PASS"
-FTP_PORT = 222
-
+FTP_PASS = "123456"
+FTP_PORT = 22
+FTP_PATH = '/path/dir/root/'
+#Ignorando certificado
 cnopts = sftp.CnOpts()
 cnopts.hostkeys = None
 
-with sftp.Connection(host=FTP_HOST, port=FTP_PORT, username=FTP_USER, password=FTP_PASS, cnopts=cnopts) as sftp:
-   print("Connection succesfully stablished ... ")
-   sftp.cwd('/export/home/38225-teste/htdocs/')  # Switch to a remote directory
-   directory_structure = sftp.listdir_attr() # Obtain structure of the remote directory
+print('Loading function')
 
-for attr in directory_structure:
-   print(attr.filename, attr)
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    global FTP_HOST
+    global FTP_USER
+    global FTP_PASS
+    global FTP_PORT
+    global cnopts
+    global sftp
+    
+    if event and event['Records']:
+        for record in event['Records']:
+            sourcebucket = record['s3']['bucket']['name']
+            sourcekey = record['s3']['object']['key']
+            print("Mostrando SourceBucket " + sourcebucket)
+            print("Mostrando SourceKey " + sourcekey)
+            
+            #Download the file to /tmp/ folder
+            filename = os.path.basename(sourcekey)
+            download_path = '/tmp/'+ filename
+            print(download_path)
+            s3.download_file(sourcebucket, sourcekey, download_path)
+            
+            os.chdir("/tmp/")
+            
+            with sftp.Connection(host=FTP_HOST, port=FTP_PORT, username=FTP_USER, password=FTP_PASS, cnopts=cnopts) as sftp:
+                print("Connection succesfully stablished ... ")
+                sftp.cwd(FTP_PATH)  # Entrando no diretorio
+                sftp.put(filename)
+            os.remove(filename)
